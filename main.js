286a04b8b01abc78fe2139d40d54b07f023f2daf
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2020-10-30 20:07:57 +0200

    final version

---
+++ main.js
@@ -0,0 +1,21 @@
+const slides = document.getElementsByClassName('slides');
+const kaire = document.querySelector('.kaire');
+const desine = document.querySelector('.desine')
+let index = 1;
+
+function plusSlides(n) {
+    showSlides(index += n);
+}
+
+function showSlides(n) {
+    if (n > slides.length) {
+        index = 1;
+    }
+    if (n < 1) {
+        index = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[index - 1].style.display = "block";
+}
